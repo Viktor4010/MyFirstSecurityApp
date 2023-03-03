231bc267e5516e75850de369a3aa9c7373f1d6c6
Viktor4010 <avacode78@gmail.com>
2023-03-03 13:42:41 +0700

    implement custom page for authentication

--- src/main/java/ru/ivanov/MyFirstSecurityApp/config/SecurityConfig.java
+++ src/main/java/ru/ivanov/MyFirstSecurityApp/config/SecurityConfig.java
@@ -3,30 +3,50 @@ package ru.ivanov.MyFirstSecurityApp.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import ru.ivanov.MyFirstSecurityApp.services.PersonDetailsService;
 
 
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-private final PersonDetailsService personDetailsService;
+
+    private final PersonDetailsService personDetailsService;
 
     @Autowired
     public SecurityConfig(PersonDetailsService personDetailsService) {
         this.personDetailsService = personDetailsService;
     }
 
-    // Set authentication
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        // конфигурируем сам Spring Security
+        // конфигурируем авторизацию
+        http.csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/auth/login","/error").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin().loginPage("/auth/login")
+                .loginProcessingUrl("/process_login")
+                .defaultSuccessUrl("/hello", true)
+                .failureUrl("/auth/login?error");
+    }
+
+    // Настраиваем аутентификацию
+    @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(personDetailsService);
     }
 
-    // указываем как шифруем наш пароль
     @Bean
     public PasswordEncoder getPasswordEncoder() {
-        return NoOpPasswordEncoder.getInstance();
+        return  NoOpPasswordEncoder.getInstance();
     }
 }
