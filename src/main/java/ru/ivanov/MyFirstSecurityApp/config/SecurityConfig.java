ced3bc56b58f2f2d03d4a638dc63cca011853bd8
Viktor4010 <avacode78@gmail.com>
2023-03-03 12:58:06 +0700

    simplify logic. remove custom AuthProviderImpl.java and deligate this simple process to Spring Security

--- src/main/java/ru/ivanov/MyFirstSecurityApp/config/SecurityConfig.java
+++ src/main/java/ru/ivanov/MyFirstSecurityApp/config/SecurityConfig.java
@@ -1,23 +1,32 @@
 package ru.ivanov.MyFirstSecurityApp.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import ru.ivanov.MyFirstSecurityApp.security.AuthProviderImpl;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import ru.ivanov.MyFirstSecurityApp.services.PersonDetailsService;
+
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-
-    private final AuthProviderImpl authProvider;
+private final PersonDetailsService personDetailsService;
 
     @Autowired
-    public SecurityConfig(AuthProviderImpl authProvider) {
-        this.authProvider = authProvider;
+    public SecurityConfig(PersonDetailsService personDetailsService) {
+        this.personDetailsService = personDetailsService;
     }
 
     // Set authentication
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.authenticationProvider(authProvider);
+        auth.userDetailsService(personDetailsService);
+    }
+
+    // указываем как шифруем наш пароль
+    @Bean
+    public PasswordEncoder getPasswordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
     }
 }
