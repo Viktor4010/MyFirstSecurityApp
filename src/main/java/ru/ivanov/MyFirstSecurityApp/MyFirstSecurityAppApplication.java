ffbebb8515af47e03fd910982eaefbc1c1a444bd
Viktor4010 <avacode78@gmail.com>
2023-03-03 11:30:47 +0700

    initial commit

---
+++ src/main/java/ru/ivanov/MyFirstSecurityApp/MyFirstSecurityAppApplication.java
@@ -0,0 +1,13 @@
+package ru.ivanov.MyFirstSecurityApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyFirstSecurityAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyFirstSecurityAppApplication.class, args);
+	}
+
+}
